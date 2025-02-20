f1984557dcb5dde277b74aeaf86f366b2463dd7b
jon <phompasit2323@gmail.com>
2025-02-20 15:11:05 +0700

    seven

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
@@ -5,24 +6,22 @@ const routers = require("./routers/routers.js");
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const connectDB = require("./database/database.js");
-require("dotenv").config();
-
-app.use(
-  cors({
-    origin: ["http://localhost:3000", "http://localhost:3001"],
-    credentials: true,
-    allowedHeaders: ["Content-Type", "Authorization"],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-  })
-);
-
-// Middleware
-app.use(bodyParser.json());
-app.use(cookieParser());
-app.use("/api", routers);
 
 // เชื่อมต่อ MongoDB
 const server_two = async () => {
+  app.use(
+    cors({
+      origin: ["http://localhost:3000", "http://localhost:3001"],
+      credentials: true,
+      allowedHeaders: ["Content-Type", "Authorization"],
+      methods: ["GET", "POST", "PUT", "DELETE"],
+    })
+  );
+
+  // Middleware
+  app.use(bodyParser.json());
+  app.use(cookieParser());
+  app.use("/api", routers);
   try {
     await connectDB(process.env.MONGODB_URL);
     console.log("MongoDB connected successfully");
@@ -31,7 +30,9 @@ const server_two = async () => {
     process.exit(1);
   }
 };
-server_two();
-
+server_two().catch((error) => {
+  console.error("Setup Error:", error);
+  process.exit(1);
+});
 // ส่งออก `app` เพื่อให้ Vercel ใช้
 module.exports = app;
