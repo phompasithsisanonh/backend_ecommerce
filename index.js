6919abe28fc80c6d3eb520310432e2f9521765fe
jon <phompasit2323@gmail.com>
2025-02-21 11:31:57 +0700

    kk

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const routers = require("./routers/routers.js");
+const routers = require("./routers/routers");
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const connectDB = require("./database/database.js");
