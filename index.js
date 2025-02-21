4616c61c1d805bfd45ad0e5a0ba14e4ebe2c94c1
jon <phompasit2323@gmail.com>
2025-02-21 11:42:01 +0700

    wwww

--- index.js
+++ index.js
@@ -7,34 +7,34 @@ const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const connectDB = require("./database/database.js");
 
-const PORT =5000;
+const PORT = 5000;
 
 // เชื่อมต่อ MongoDB
 
-  app.use(
-    cors({
-      origin: ["http://localhost:3000", "http://localhost:3001"],
-      credentials: true,
-      allowedHeaders: ["Content-Type", "Authorization"],
-      methods: ["GET", "POST", "PUT", "DELETE"],
-    })
-  );
-  // Middleware
-  app.use(bodyParser.json());
-  app.use(cookieParser());
-  app.use("/", routers);
-const connectDBd= async()=>{
+const connectDBd = async () => {
   try {
+    app.use(
+      cors({
+        origin: ["http://localhost:3000", "http://localhost:3001"],
+        credentials: true,
+        allowedHeaders: ["Content-Type", "Authorization"],
+        methods: ["GET", "POST", "PUT", "DELETE"],
+      })
+    );
+    // Middleware
+    app.use(bodyParser.json());
+    app.use(cookieParser());
+    app.use("/", routers);
     await connectDB(process.env.MONGODB_URL);
     console.log("MongoDB connected successfully");
   } catch (error) {
     console.error("DB connection error:", error);
     process.exit(1);
   }
-}
-connectDBd()
-  app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-  });
+};
+connectDBd();
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 // ส่งออก `app` เพื่อให้ Vercel ใช้
 module.exports = app;
