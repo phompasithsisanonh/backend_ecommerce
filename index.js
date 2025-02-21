65770260ffb21284209d41e4f7803c20496773c1
jon <phompasit2323@gmail.com>
2025-02-21 11:06:39 +0700

    rtrt

--- index.js
+++ index.js
@@ -7,23 +7,23 @@ const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const connectDB = require("./database/database.js");
 
-const PORT = 5000;
+const PORT =5000;
 
 // เชื่อมต่อ MongoDB
 
-app.use(
-  cors({
-    origin: ["http://localhost:3000", "http://localhost:3001"],
-    credentials: true,
-    allowedHeaders: ["Content-Type", "Authorization"],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-  })
-);
-// Middleware
-app.use(bodyParser.json());
-app.use(cookieParser());
-app.use("/api", routers);
-const connectDBd = async () => {
+  app.use(
+    cors({
+      origin: ["http://localhost:3000", "http://localhost:3001"],
+      credentials: true,
+      allowedHeaders: ["Content-Type", "Authorization"],
+      methods: ["GET", "POST", "PUT", "DELETE"],
+    })
+  );
+  // Middleware
+  app.use(bodyParser.json());
+  app.use(cookieParser());
+  app.use("/api", routers);
+const connectDBd= async()=>{
   try {
     await connectDB(process.env.MONGODB_URL);
     console.log("MongoDB connected successfully");
@@ -31,10 +31,13 @@ const connectDBd = async () => {
     console.error("DB connection error:", error);
     process.exit(1);
   }
-};
-connectDBd();
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+}
+app.use((req, res) => {
+  res.status(404).send("Route not found");
 });
+connectDBd()
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
 // ส่งออก `app` เพื่อให้ Vercel ใช้
 module.exports = app;
