3d31785e1fd8df1c6925ba03bcb00611b657df08
jon <phompasit2323@gmail.com>
2025-02-22 14:59:05 +0700

    dsa

--- server.js
+++ server.js
@@ -11,35 +11,31 @@ const server = http.createServer(app);
 const PORT = process.env.PORT || 5000;
 
 // เชื่อมต่อ MongoDB
+// ใช้ CORS และ Middleware
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "http://localhost:3001"], // ตั้งค่าการเข้าถึง
+    credentials: true,
+    allowedHeaders: ["Content-Type", "Authorization"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use("/api", routers); // ตั้งค่า router หลัก
 const connectDBd = async () => {
   try {
-    await connectDB(process.env.MONGODB_URL);  // เชื่อมต่อ MongoDB
+    await connectDB(process.env.MONGODB_URL); // เชื่อมต่อ MongoDB
     console.log("MongoDB connected successfully");
-
-    // ใช้ CORS และ Middleware
-    app.use(
-      cors({
-        origin: ["http://localhost:3000", "http://localhost:3001"],  // ตั้งค่าการเข้าถึง
-        credentials: true,
-        allowedHeaders: ["Content-Type", "Authorization"],
-        methods: ["GET", "POST", "PUT", "DELETE"],
-      })
-    );
-    app.use(bodyParser.json());
-    app.use(cookieParser());
-    app.use("/api", routers);  // ตั้งค่า router หลัก
-
-    server.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}`);
-    });
-
   } catch (error) {
     console.error("DB connection error:", error);
     process.exit(1);
   }
 };
 
-connectDBd();  // เรียกใช้ฟังก์ชั่นเชื่อมต่อกับฐานข้อมูล
-
+connectDBd(); // เรียกใช้ฟังก์ชั่นเชื่อมต่อกับฐานข้อมูล
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 // ส่งออก `app` ให้ Vercel ใช้
-module.exports = server ;
+module.exports = server;
